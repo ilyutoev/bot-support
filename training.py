cb0fa7acaf3a8014c398745d3410c3e712df6520
ilyutoev <ilyutoev@gmail.com>
2020-10-06 14:09:33 +0300

    Открываем файл через контекстный менеджер

--- training.py
+++ training.py
@@ -8,23 +8,24 @@ dialog_project_id = os.getenv('DIALOG_PROJECT_ID')
 
 def create_intents():
     """Создаем интенты с вопросами и ответами из файла questions.json"""
-    questions = json.load(open('questions.json', 'r'))
-    client = dialogflow_v2.IntentsClient()
-    parent = client.project_agent_path(dialog_project_id)
+    with open('questions.json', 'r') as qf:
+        questions = json.load(qf)
+        client = dialogflow_v2.IntentsClient()
+        parent = client.project_agent_path(dialog_project_id)
 
-    for title, question in questions.items():
-        intent = {
-            "display_name": title,
-            "messages": [{"text": {"text": [question['answer']]}}],
-            "training_phrases": [{"parts": [{"text": q_text}]} for q_text in question.get('questions')]
-        }
-        try:
-            client.create_intent(parent, intent)
-        except Exception as e:
-            if 'already exists' in str(e):
-                print('Интент уже существует')
-            else:
-                raise e
+        for title, question in questions.items():
+            intent = {
+                "display_name": title,
+                "messages": [{"text": {"text": [question['answer']]}}],
+                "training_phrases": [{"parts": [{"text": q_text}]} for q_text in question.get('questions')]
+            }
+            try:
+                client.create_intent(parent, intent)
+            except Exception as e:
+                if 'already exists' in str(e):
+                    print('Интент уже существует')
+                else:
+                    raise e
 
 
 def train_agent():
